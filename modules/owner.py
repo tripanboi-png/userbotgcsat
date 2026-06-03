6586a7460de71aa3714f9ccbe65991d36d9ad477
tripanboi-png/ <tripanboi@gmail.com>
2026-06-03 08:41:00 +0700

    Update owner.py

--- modules/owner.py
+++ modules/owner.py
@@ -104,7 +104,7 @@ def register(client, name: str = "owner"):
 
         LAST_HELP_MESSAGE = await event.respond(help_text.strip())
 
-         try:
+    try:
              await event.delete()
          except:
              pass
